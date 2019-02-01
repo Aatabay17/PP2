51b2f4c02b7bad8489b367e0ecc91a23c48c7a37
Alim <alim.atabay117@gmail.com>
2019-02-01 12:04:42 +0600

    asd

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            string a2 = Console.ReadLine();
+
+            int n = int.Parse(a);
+            //string[] numsStr = a2.Split(new char[] { ',', ' ', ';', '#', '$' });
+            string[] numsStr = a2.Split();
+
+            for (int i = 0; i < numsStr.Length; ++i)
+            {
+                int x = int.Parse(numsStr[i]);
+                for (int j = 0; j < 2; ++j)
+                {
+                    Console.Write(x + " ");
+                }
+       
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+
+        public static void f(int x)
+        {
+            bool ok = false;
+            for (int i = 2; i <= Math.Sqrt(x); i++)
+            {
+                if (x % i == 0)
+                {
+                    ok= true;
+                }
+                if (!ok) Console.WriteLine(x);
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            string a1 = Console.ReadLine();
+            List<string> pr = new List<string>();
+            int n = int.Parse(a);
+
+            string[] arr = a1.Split();
+            for(int i = 0; i < arr.Length; i++)
+            {
+                int b = int.Parse(arr[i]);
+                if (ok=true)
+                {
+                    List<string>.pr.Add(a[i]);
+                }
+                List<string>.pr.Count(a[i]);
+            }
+            
+            Console.ReadKey();
+        }
+        
+        }
+    }
