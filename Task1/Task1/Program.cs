a2eab9e7de39fa540658515edd1bf26d57efaa82
Alim <alim.atabay117@gmail.com>
2019-02-01 12:18:06 +0600

    first commit

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        public static bool Prime(int a)//создаем функцию для проверки простых чисел
+        {
+            bool ch = true;
+            if (a == 1) ch = false;
+            for (int i = 2; i < a; i++)//если число простое,надо проверить не делится ли число до него
+            {
+                if (a % i == 0)
+                {
+                    ch = false;
+                }
+            }
+            return ch;
+        }
+        static void Main(string[] args)
+        {
+
+            int n;//создание переменной
+            n = int.Parse(Console.ReadLine());//ввожу массив
+            string s = Console.ReadLine();
+            string[] arr = s.Split();//создаем массив и разибиваю строку s элементы массива
+            int cnt = 0;//создаем переменную для по
+            for (int i = 0; i < n; i++)
+            {
+                int c = int.Parse(arr[i]);//преобразовываем элемент массива в число 
+                if (Prime(c) == true)//проверка числа
+                {
+                    cnt++;
+                }
+            }
+            Console.WriteLine(cnt);//выводим счетчик
+
+            for (int i = 0; i < n; i++)
+            {
+                int c = int.Parse(arr[i]);
+                if (Prime(c) == true)
+                {
+                    Console.WriteLine(c + " ");//выводим на экран простые числа через пробел в строке
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task3/Task3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            string a2 = Console.ReadLine();
+
+            int n = int.Parse(a);
+
+            string[] numsStr = a2.Split();
+
+            for (int i = 0; i < numsStr.Length; ++i)
+            {
+                int x = int.Parse(numsStr[i]);
+                for (int j = 0; j < 2; ++j)
+                {
+                    Console.Write(x + " ");
+                }
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
